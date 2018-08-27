fe2eccf78cd2b33fd6e54eba346fc614d088e68a
Tomaš Monkevič <TomasMo@office.local>
2018-08-27 17:08:04 +0300

    Test found words are stored in file

--- BoggleSolverTests/Tests/SolverTests.cpp
+++ BoggleSolverTests/Tests/SolverTests.cpp
@@ -4,8 +4,20 @@
 #include <Tree.h>
 #include <Timer.h>
 #include <BoggleSolver.h>
+#include <fstream>
 
 using namespace TomasMo;
+
+std::ostream& operator<<(std::ostream& os, const Results& obj)
+{
+	os << "Count: " << obj.Count << std::endl;
+	os << "Score: " << obj.Score << std::endl;
+	for (unsigned i = 0; i < obj.Count; i++)
+	{
+		os << obj.Words[i] << " ";
+	}
+	return os;
+}
 
 class SolverTests : public ::testing::Test 
 {
@@ -32,6 +44,9 @@ TEST_F(SolverTests, Board3x3)
 	Timer timer;
 	Results result = solver.FindWords();
 	long long elapsedMs = timer.ElapsedMs();
+	std::ofstream resultFile(std::string(RESOURCE_PATH) + std::string("Board3x3-Results.txt"));
+	resultFile << result;
+	resultFile.close();
 	FreeWords(result);
 	EXPECT_EQ(result.Count, 21);
 	std::cout << "Time elapsed: " << elapsedMs << " ms. " << result.Count << std::endl;
@@ -44,6 +59,9 @@ TEST_F(SolverTests, Board100x100)
 	Timer timer;
 	Results result = solver.FindWords();
 	long long elapsedMs = timer.ElapsedMs();
+	std::ofstream resultFile(std::string(RESOURCE_PATH) + std::string("Board100x100-Results.txt"));
+	resultFile << result;
+	resultFile.close();
 	FreeWords(result);
 	//EXPECT_EQ(result.Count, 21);
 	std::cout << "Time elapsed: " << elapsedMs << " ms." << result.Count << std::endl;
@@ -56,6 +74,9 @@ TEST_F(SolverTests, Board300x300)
 	Timer timer;
 	Results result = solver.FindWords();
 	long long elapsedMs = timer.ElapsedMs();
+	std::ofstream resultFile(std::string(RESOURCE_PATH) + std::string("Board300x300-Results.txt"));
+	resultFile << result;
+	resultFile.close();
 	FreeWords(result);
 	//EXPECT_EQ(result.Count, 21);
 	std::cout << "Time elapsed: " << elapsedMs << " ms." << result.Count << std::endl;
@@ -68,6 +89,9 @@ TEST_F(SolverTests, Board500x500)
 	Timer timer;
 	Results result = solver.FindWords();
 	long long elapsedMs = timer.ElapsedMs();
+	std::ofstream resultFile(std::string(RESOURCE_PATH) + std::string("Board500x500-Results.txt"));
+	resultFile << result;
+	resultFile.close();
 	FreeWords(result);
 	//EXPECT_EQ(result.Count, 21);
 	std::cout << "Time elapsed: " << elapsedMs << " ms." << result.Count << std::endl;
@@ -80,6 +104,9 @@ TEST_F(SolverTests, Board1000x1000)
 	Timer timer;
 	Results result = solver.FindWords();
 	long long elapsedMs = timer.ElapsedMs();
+	std::ofstream resultFile(std::string(RESOURCE_PATH) + std::string("Board1000x1000-Results.txt"));
+	resultFile << result;
+	resultFile.close();
 	FreeWords(result);
 	//EXPECT_EQ(result.Count, 21);
 	std::cout << "Time elapsed: " << elapsedMs << " ms." << result.Count << std::endl;
